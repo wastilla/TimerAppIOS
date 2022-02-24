b06478a56b880a29d426aa95e86102b8ac5d3816
wastilla <wastilla@email.unc.edu>
2022-02-24 17:53:07 -0500

    Initial Commit

---
+++ Timer App/Timer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_AppApp.swift
+//  Timer App
+//
+//  Created by will astilla on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Timer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
